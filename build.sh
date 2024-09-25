1dbcdbcd7fb394defb58d53259fbf2bd8c9a5e76
Chip Shabazian <chip@shabazian.com>
2024-09-24 19:05:45 -0700

    fixed RHEL94 variable in build.sh

--- build.sh
+++ build.sh
@@ -13,6 +13,6 @@ read -p "What is your offline token? " offline_token
 # Get a download token:
 token=$(curl https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token -d grant_type=refresh_token -d client_id=rhsm-api -d refresh_token=$offline_token | jq --raw-output .access_token)
 # Use the Red Hat Enterprise Linux 9.4 Binary DVD SHA-256 Checksum from https://access.redhat.com/downloads/content/rhel
-RHEL9.4="398561d7b66f1a4bf23664f4aa8f2cfbb3641aa2f01a320068e86bd1fc0e9076"
+RHEL94="398561d7b66f1a4bf23664f4aa8f2cfbb3641aa2f01a320068e86bd1fc0e9076"
 # Download the ISO:
-curl -H "Authorization: Bearer $token" -L https://api.access.redhat.com/management/v1/images/$RHEL/download -o rhel-9.4-x86_64-dvd.iso
+curl -H "Authorization: Bearer $token" -L https://api.access.redhat.com/management/v1/images/$RHEL94/download -o rhel-9.4-x86_64-dvd.iso

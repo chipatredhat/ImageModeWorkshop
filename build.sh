70a803bdc11c75e47ea2c49f13dc45b0e7772068
Chip Shabazian <chip@shabazian.com>
2024-09-26 10:26:02 -0700

    Added the ability to pass the online token as a variable to allow a fully automated deployment

--- build.sh
+++ build.sh
@@ -3,13 +3,17 @@
 # Make sure you can get passwordless root
 [[ $(sudo -A whoami) != "root" ]] && echo "You need to have passwordless sudo access" && exit
 
-# Install ansible-core if necessary, and then do the rest in the playbook
+# Install ansible-core if necessary, and then do what we can in the playbook
 [[ $(command -v ansible-playbook) ]] || sudo dnf -y install ansible-core
 
 # Download the RHEL ISO using a token:
 # Put together based on Ansible instructions at https://access.redhat.com/solutions/7013662
+if [ "$1" = "" ] ; then
 echo -e "\n\nGet an offline API token from https://access.redhat.com/management/api\n"
 read -p "What is your offline token? " offline_token
+else
+offline_token=$1
+fi
 # Get a download token:
 token=$(curl https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token -d grant_type=refresh_token -d client_id=rhsm-api -d refresh_token=$offline_token | jq --raw-output .access_token)
 # Use the Red Hat Enterprise Linux 9.4 Binary DVD SHA-256 Checksum from https://access.redhat.com/downloads/content/rhel

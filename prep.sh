9c7b7c7d48665e77b28377785d887b4b67640444
Chip Shabazian <chip@shabazian.com>
2024-09-26 13:11:17 -0700

    Added token capability to prep.sh

--- prep.sh
+++ prep.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # This prep script will install git and ansible core, then clone the repo and start the build
-# You can use it by running: curl -s 
+# You can use it by running: curl -s https://raw.githubusercontent.com/chipatredhat/ImageModeWorkshop/refs/heads/main/prep.sh | bash
+# Or pass your token with: curl -s https://raw.githubusercontent.com/chipatredhat/ImageModeWorkshop/refs/heads/main/prep.sh | bash -s -- <your_token>
 sudo dnf -y install git ansible-core
 git clone https://github.com/chipatredhat/ImageModeWorkshop.git
 cd ImageModeWorkshop

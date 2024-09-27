80410deee18bec1ad3e9e69cc988d8c4839c7fa0
Chip Shabazian <chip@shabazian.com>
2024-09-27 13:50:53 -0700

    updated prep.sh instructions

--- prep.sh
+++ prep.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This prep script will install git and ansible core, then clone the repo and start the build
 # You can use it by running: curl -s https://raw.githubusercontent.com/chipatredhat/ImageModeWorkshop/refs/heads/main/prep.sh | bash
-# Or pass your token with: curl -s https://raw.githubusercontent.com/chipatredhat/ImageModeWorkshop/refs/heads/main/prep.sh | bash -s -- <your_token>
+# Or pass your variables  with: curl -s https://raw.githubusercontent.com/chipatredhat/ImageModeWorkshop/refs/heads/main/prep.sh | bash -s -- <your_token> <RH_REGISTRY_ACCOUNT> <RH_REGISTRY_TOKEN/PASSWORD>
 sudo dnf -y install git ansible-core
 git clone https://github.com/chipatredhat/ImageModeWorkshop.git
 cd ImageModeWorkshop

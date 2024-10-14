f65ecd978fb01495f83d79989611bc1146a74932
Chip Shabazian <chip@shabazian.com>
2024-10-14 15:56:42 -0700

    added start_imagemode.sh

---
+++ files/start_imagemode.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+if [ "$1" = "" ] ; then read -p "What is the name of the system you wish to start? " SYSNAME ; else SYSNAME=$1 ; fi
+
+virt-install --pxe --network network=default --name ${SYSNAME}  --memory 4096 --disk size=20 --nographics --boot menu=on,useserial=on --osinfo rhel9.4

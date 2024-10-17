7077a3d8a5a64887bb28c2fda0b7045d6533cb4e
Chip Shabazian <chip@shabazian.com>
2024-10-17 14:47:00 -0700

    added restart to application_install.sh

--- files/application_install.sh
+++ files/application_install.sh
@@ -2,3 +2,4 @@
 curl -s http://kickstart.example.com/petclinic/petclinic.sql -o /tmp/petclinic.sql
 sudo mysql -uroot -e "CREATE DATABASE petclinic"
 sudo mysql -uroot petclinic < /tmp/petclinic.sql
+sudo systemctl restart petclinic

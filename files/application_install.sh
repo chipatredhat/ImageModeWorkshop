66d7ae9d12dae92af12e2f6cc78e656c16cb14e4
Chip Shabazian <chip@shabazian.com>
2024-10-17 14:44:35 -0700

    added sudo

--- files/application_install.sh
+++ files/application_install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 curl -s http://kickstart.example.com/petclinic/petclinic.sql -o /tmp/petclinic.sql
-mysql -uroot -e "CREATE DATABASE petclinic"
-mysql -uroot petclinic < /tmp/petclinic.sql
+sudo mysql -uroot -e "CREATE DATABASE petclinic"
+sudo mysql -uroot petclinic < /tmp/petclinic.sql

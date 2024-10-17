305eb6044a752acab5e1100c1e4acf2735518535
Chip Shabazian <chip@shabazian.com>
2024-10-17 15:16:11 -0700

    moved application_install.sh to the kickstart file

--- files/application_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-curl -s http://kickstart.example.com/petclinic/petclinic.sql -o /tmp/petclinic.sql
-sudo mysql -uroot -e "CREATE DATABASE petclinic"
-sudo mysql -uroot petclinic < /tmp/petclinic.sql
-sudo systemctl restart petclinic

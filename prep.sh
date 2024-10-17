312c5a0caeb57fcb8b22b066995fdc305081e055
Chip Shabazian <chip@shabazian.com>
2024-10-17 13:33:05 -0700

    Moved git clone to /tmp

--- prep.sh
+++ prep.sh
@@ -3,6 +3,7 @@
 # You can use it by running: curl -s https://raw.githubusercontent.com/chipatredhat/ImageModeWorkshop/refs/heads/main/prep.sh | bash
 # Or pass your variables  with: curl -s https://raw.githubusercontent.com/chipatredhat/ImageModeWorkshop/refs/heads/main/prep.sh | bash -s -- <your_token> <RH_REGISTRY_ACCOUNT> <RH_REGISTRY_TOKEN/PASSWORD>
 sudo dnf -y install git ansible-core
+cd /tmp
 git clone https://github.com/chipatredhat/ImageModeWorkshop.git
 cd ImageModeWorkshop
 ./build.sh $1 $2 $3

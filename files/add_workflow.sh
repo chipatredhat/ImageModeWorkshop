a9a8b58f8645bc7347685932bcaae1cc991ee793
chipatredhat <chip@redhat.com>
2025-08-26 15:18:56 -0700

    Update add_workflow.sh

--- files/add_workflow.sh
+++ files/add_workflow.sh
@@ -2,5 +2,5 @@
 
 cd /tmp
 git clone https://github.com/chipatredhat/Image-Mode-Demo.git
-#ansible-galaxy collection install -r ./Image-Mode-Demo/ansible/roles/requirements.yml
+ansible-galaxy collection install -r ./Image-Mode-Demo/ansible/roles/requirements.yml
 ansible-playbook Image-Mode-Demo/ansible/setup-demo.yml 
